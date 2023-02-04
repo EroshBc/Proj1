755ada809e0a4335d5e414610c349515a365a4bd
Erosh Boralugodage <erosh.bc@gmail.com>
2023-02-04 03:27:20 -0600

    first commit Create Make/oss and worker files

---
+++ Proj1/oss.c
@@ -0,0 +1,13 @@
+//
+//  oss.c
+//  Proj1
+//
+//  Created by Erosh Boralugodage on 2/4/23.
+//
+//
+#include <stdio.h>
+
+int main(){
+    printf("Welcome oss");
+    return 0;
+}
---
+++ Proj1/worker.c
@@ -0,0 +1,13 @@
+//
+//  worker.c
+//  Proj1
+//
+//  Created by Erosh Boralugodage on 2/4/23.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("Welcome worker");
+    return 0;
+}
