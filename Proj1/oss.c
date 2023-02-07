47ba81c4d23e9620e6371093f6e9f0c4d4a3ae3f
Erosh Boralugodage <erosh.bc@gmail.com>
2023-02-07 03:51:52 -0600

    commit on ross - create oss base

--- Proj1/oss.c
+++ Proj1/oss.c
@@ -6,8 +6,32 @@
 //
 //
 #include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
-    printf("Welcome oss");
+int main(int argc, char* argv[]){
+    printf("Welcome oss\n");
+    int n=0, s=0, t=0;
+    int option=0;
+    
+    while((option = getopt(argc, argv, "hn:s:t") != -1)){
+        switch(option){
+            case 'h':
+                printf("help");
+            case 'n':
+                //get the value of n
+            case 's':
+                //get the value of s
+            case 't':
+                //get the value of t
+            case '?':
+                //else exit
+        }
+                
+                
+        }
+    
+
     return 0;
 }
